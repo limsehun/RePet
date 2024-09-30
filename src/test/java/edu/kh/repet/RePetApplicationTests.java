b7b491f3b89b31aa940cf49d32bf601bdbc93846
limsehun <tpgns8714852@gmail.com>
2024-09-30 14:13:00 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/edu/kh/repet/RePetApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.repet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

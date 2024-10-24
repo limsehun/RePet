46a0a4408e5ec1cf46a662ea8e35d79c709a9fc8
sungyeopkim11 <777sy777@naver.com>
2024-10-24 14:25:52 +0900

    20241024 자유 게시판 관련 페이지 커밋

---
+++ src/main/java/edu/kh/repet/board/dto/Board.java
@@ -0,0 +1,60 @@
+package edu.kh.repet.board.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Board {
+
+
+//BOARD 테이블 컬럼과 매핑되는 필드
+
+//행 번호
+	private int rnum;
+	
+	// BOARD 테이블 컬럼과 매핑되는 필드
+
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardWriteDate;
+	private String boardUpdateDate;
+	private int readCount;
+	private String boardDelFl;
+	private int memberNo;
+	private int boardCode;
+	
+
+	private String memberNickname;
+
+	//목록 조회 시 댓글/ 좋아요 수 상관 쿼리 결과
+	private int commentCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

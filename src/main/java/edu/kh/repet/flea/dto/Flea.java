ca0a71eee4500e07a42287c1f64c3063749508e0
LYoungSoo <leeyeong04@gmail.com>
2024-10-24 18:02:19 +0900

    채팅 DB 읽어오기 만들어야... 구현이 너무 빡센데

--- src/main/java/edu/kh/repet/flea/dto/Flea.java
+++ src/main/java/edu/kh/repet/flea/dto/Flea.java
@@ -1,4 +1,28 @@
 package edu.kh.repet.flea.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+//@Builder
 public class Flea {
+	
+	private int		boardNo;
+	private String	boardTitle;
+	private String	boardContent;
+	private String  memberNickname;
+	private int		memberNo;
+	private int		chatRoomCount;
+	private String	boardWriteDate;
+	private String	boardUpdateDate;
+	private String	address;
+	private String	thumbnail;
+	private String	profileImg;
+	private int 	price;
 }
--- src/main/java/edu/kh/repet/main/controller/MainController.java
+++ src/main/java/edu/kh/repet/main/controller/MainController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
-
 import edu.kh.repet.main.service.MainService;
 import lombok.RequiredArgsConstructor;
 

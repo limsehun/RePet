38dc3d3f41bfa9e73a703dc29e2efb010b8173d0
DESKTOP-RIENOSP\user1 <Sum-yujin>
2024-10-25 17:48:49 +0900

    241025_더보기 버튼

---
+++ src/main/java/edu/kh/repet/adopt/service/AdoptService.java
@@ -0,0 +1,12 @@
+package edu.kh.repet.adopt.service;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+
+public interface AdoptService {
+
+	Map<String, Object> selectAdoptList(String urlStr, int cp) throws IOException;
+
+}
--- src/main/resources/static/js/adopt/main.js
+++ src/main/resources/static/js/adopt/main.js
@@ -1,22 +1,110 @@
 
-const plusBtn = document.querySelector("#plusBtn");
-const adoptList = document.querySelector(".adoptList");
+const moreBtn = document.getElementById('more-adopt-card');
+const adoptListAll = document.querySelector(".adopt-list")
 
-pageNoList?.forEach((item,index) => {
+moreBtn.addEventListener("click", () =>{
+  selectAdoptList();
+})  
 
-})
+const selectAdoptList = (map) => {
 
+  let selectUrl ="/adopt/selectAdoptList"
 
-plusBtn.addEventListener("click",()=>{
+  if(cp !== undefined){
+    selectUrl += `&cp=${cp}`;
+  }
 
-  fetch("/adopt/main)
-  .then(response =>{
-    if(response.ok) return response.text();
-    throw new Error("불러오기 실패")
+  fetch("selectUrl")
+  .then(response => {
+    if(response.ok) return response.json();
+    throw new Error("조회 실패");
   })
-  .then(html => {
-    console.log(html);
-  })
-  .catch(err => console.error(err));
-})
+  .then(map => {
 
+    const adoptList = map.adoptList;
+    const pagination = map.pagination;
+
+    console.log(pagination)
+    console.log(adoptList)
+
+    adoptList.forEach( adopt => {
+        const petCard = document.createElement('div');
+        petCard.className = 'pet-card';
+        petCard.innerHTML = `
+          <div class="pet-image">
+              <img class="open-api-img" src="${adopt.popfile ? adoptList.popfile : '/images/default.png'}" />
+          </div>
+          <div class="content">
+            <div class="condition">${adopt.processState}</div>
+            <p>
+              <strong>
+                ${adopt.kindCd} / ${adopt.sexCd}
+              </strong>
+            </p>
+            <p>
+              나이: ${adopt.age}
+             </p>
+          </div>
+        `;
+        adoptListAll.appendChild(petCard);
+    });
+
+  })
+  .catch(err=>console.log(err));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    selectAdoptList();
+});
+
+
+// moreBtn.addEventListener('click',function() {
+//   adoptList();
+// })
+
+// const div = document.createElement('div');
+// const span = document.createElement('strong');
+// const p = document.createElement('p');
+// const img = document.createElement('img');
+
+// function adoptList() {
+//   fetch("/adopt/main")
+//   .then(response => {
+//     if(response.ok) return response.json();
+//     throw new Error("조회 실패");
+//   })
+//   .then(html => {
+//     console.log(html);
+//     displayPets(adoptList);
+//   })
+//   .catch(err=>console.log(err));
+// }; 
+
+
+// function displayPets(adoptList) {
+//   const petList = document.getElementById('pet-list');
+
+//   adoptList.forEach(adopt => {
+//       const petCard = document.createElement('div');
+//       petCard.className = 'pet-card';
+//       petCard.innerHTML = `
+//           <div class="pet-image">
+//               <img class="open-api-img" src="${adopt.popfile ? adopt.popfile : '/images/default.png'}" />
+//           </div>
+//           <div class="content">
+//               <div class="condition">${adopt.processState}</div>
+//               <p>
+//                   <strong>
+//                       ${adopt.kindCd} / ${adopt.sexCd}
+//                   </strong>
+//               </p>
+//               <p>
+//                   나이: ${adopt.age}
+//               </p>
+//           </div>
+//       `;
+//       petList.appendChild(petCard);
+//   });
+
+  
+// };

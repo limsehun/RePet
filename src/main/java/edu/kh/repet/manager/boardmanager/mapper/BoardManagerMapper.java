bdc5815f74aeef202796544136b7780488db1de9
ldxhyeon <dlehdgus3180@naver.com>
2024-11-01 10:52:23 +0900

    관리자페이지

--- src/main/java/edu/kh/repet/manager/boardmanager/mapper/BoardManagerMapper.java
+++ src/main/java/edu/kh/repet/manager/boardmanager/mapper/BoardManagerMapper.java
@@ -2,10 +2,8 @@ package edu.kh.repet.manager.boardmanager.mapper;
 
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 import edu.kh.repet.board.dto.Board;
@@ -15,22 +13,19 @@ import edu.kh.repet.board.dto.ReportBoard;
 public interface BoardManagerMapper {
 
 	// 게시물 수
-	int boardCount(@Param("key") String key, @Param("query") String query);
+	int boardCount();
 
 	// 게시물 전체 조회
+	List<Board> selectBoardList(RowBounds rowBounds);
 
 	// 게시물 삭제
 	int deleteBoard(int boardNo);
-
-	List<Board> selectBoardList(@Param("key") String key, @Param("query") String query, RowBounds rowBounds);
-
 
 	// 신고 누적 수
 	int reportCount();
 
 	// 신고 게시물 조회
 	List<ReportBoard> reportBoardList(RowBounds rowBounds);
-
 
 
 }
